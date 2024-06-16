95c6825a581c66006b288468cbb0f2a1a3d6dcb7
Israel Sanchez <isanchez0746@gmail.com>
2024-06-16 18:02:24 -0500

    a few videos into the tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # GET /about
+  get "about-us", to: "about#index", as: :about
+
+  root to: "main#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
